ed1486a07f62953218a5f41d47a6bc49132eea61
Adrian Lopez <adrian.lopez@pucp.edu.pe>
2023-06-20 19:16:53 -0500

    1 commit

---
+++ src/main/java/com/example/lab9_20176054/Lab920176054Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20176054;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920176054Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920176054Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab9_20176054/Lab920176054ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20176054;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920176054ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
